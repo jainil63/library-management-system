5466acb83385df869987c54f8c92eabda4c13b2c
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-12-13 16:59:33 +0530

    made a static webserver

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,28 @@
 from fastapi import FastAPI, Response
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
-app.mount("/", StaticFiles(directory="./frontend"), name="frontend")
+
+app = FastAPI(
+    title="LIBIFY API",
+    description="Library Management System",
+    version="0.1.0",
+    license_info={ "name": "MIT", "identifier": "MIT"}
+)
+
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
+
+
+@app.get("/")
+def root():
+    return RedirectResponse("/frontend/index.html")
+
+
+@app.get("/api/v1")
+def api_homepage():
+    return {
+        "apiname": "LIBIFY API - Library Management System",
+        "version": "0.1.0",
+        "homepage": "/index.html",
+        "documentation": "/docs"
+    }
