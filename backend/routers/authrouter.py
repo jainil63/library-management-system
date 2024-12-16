11fa282b8f8e8570acfce619705c76b769b75160
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-12-16 17:46:30 +0530

    Update authrouter.py to support form data

--- backend/routers/authrouter.py
+++ backend/routers/authrouter.py
@@ -1,16 +1,30 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Form
+from pydantic import BaseModel
 
-from ..models import User
+from typing import Annotated
+
+
+class LoginFormData(BaseModel):
+    username: str
+    password: str
+    model_config = {"extra": "forbid"}
+
+
+class CreateAccountFormData(BaseModel):
+    email: str
+    username: str
+    password: str
+    model_config = {"extra": "forbid"}
 
 
 auth_router = APIRouter()
 
 
 @auth_router.get("/login")
-def login():
+def login(data: Annotated[LoginFormData, Form()]):
     raise NotImplementedError()
 
 
 @auth_router.post("/signup")
-def signup():
+def signup(data: Annotated[CreateAccountFormData, Form()]):
     raise NotImplementedError()
