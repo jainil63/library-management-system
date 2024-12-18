b9c09afa04edee097ff69f2f015222854bef8660
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-12-18 11:20:21 +0530

    Update database.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.utils import get_openapi
 
 from .api import api_router
-from .database import Database
+from . import database
 from .metadata import Config
 from .custom_schema import modify_openapi_schema
 
@@ -14,10 +14,14 @@ app = FastAPI(
     version=Config.API_VERSION,
     summary=Config.APP_SUMMARY,
     description=Config.APP_DESCRIPTION,
-    license_info=Config.LICENSE_INFO,
-    on_startup=[Database.init],
-    on_shutdown=[Database.close]
+    license_info=Config.LICENSE_INFO
 )
+
+
+@app.on_event("startup")
+def startup_event():
+    database.init_db()
+    print("Database initialized!!!")
 
 
 # Ref:- https://fastapi.tiangolo.com/how-to/extending-openapi/#overriding-the-defaults
--- backend/database.py
+++ backend/database.py
@@ -4,50 +4,19 @@ import sqlite3
 from .metadata import Config
 
 
-class Database:
-    instance = None
-    
-    @staticmethod
-    def init(name = ":memory:"):
-        db = Database(name)
-        db.connect()
-        db.cursor.executescript(Config.INIT_DB_SQL)
-        Database.instance = db
-    
-    @staticmethod
-    def get_db():
-        if not Database.instance.isactive:
-            Database.instance.connect()
-        return Database.instance
-    
-    @staticmethod
-    def close():
-        Database.instance.disconnect()
-    
-    def __init__(self, name: str):
-        self.name = name
-        self.conn = None
-        self.cursor = None
-        self.isactive = False
-    
-    def connect(self):
-        self.conn = sqlite3.connect(self.name)
-        self.cursor = self.conn.cursor()
-        self.isactive = True
-        print("Successfully connected with database!!!")
-        return self
-    
-    def execute_sql(self, sqlstatement, *args):
-        if not self.isactive:
-            raise Exception("Database is not connected!!")
-        
-        self.cursor.execute(sqlstatement, *args)
-        self.conn.commit()
-        return self.cursor.fetchall()
+def init_db():
+    conn = sqlite3.connect(Config.DATABASE_URL)
+    cursor = conn.cursor()
+    cursor.executescript(Config.INIT_DB_SQL)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-    def disconnect(self):
-        self.isactive = False
-        self.cursor.close()
-        self.conn.close()
-        print("Successfully disconnected with database!!!")
 
+def get_db():
+    conn = sqlite3.connect(Config.DATABASE_URL)
+    conn.row_factory = sqlite3.Row
+    try:
+        yield conn
+    finally:
+        conn.close()
