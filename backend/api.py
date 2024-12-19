f3f1178cbed2e59b05f1566576785797e5564d62
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-12-19 21:46:28 +0530

    created a test route

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Request, status
 
 from .metadata import Config
 from .routers.userrouter import user_router
 from .routers.authrouter import auth_router
-
+from .utils import verify_and_decode_token
 
 api_router = APIRouter()
 
@@ -15,6 +15,19 @@ def api_root():
         "version": Config.API_VERSION
     }
 
+
+@api_router.get("/profile", status_code=status.HTTP_200_OK)
+def query(request: Request):
+    token = request.cookies.get("access-token")
+    
+    if not token:
+        return "You are not login!!"
+    
+    payload = verify_and_decode_token(token)
+    if payload == False:
+        return "Invalid token!!"
+    
+    return { "token": token, "user": payload }
 
 api_router.include_router(user_router, prefix="/users", tags=["Backend", "Users"])
 api_router.include_router(auth_router, prefix="/auth", tags=["Backend", "Auth"])
